7e44db53845c741081c9944ea997b1d88b2d629a
AB-Siddique278 <absiddique1060@gmail.com>
2022-09-02 01:42:52 +0600

    setup all

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// echo "# Own-API-Data" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/AB-Siddique278/Own-API-Data.git
+// git push -u origin main
